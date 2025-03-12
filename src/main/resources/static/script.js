616152c298739b8e6de030d3bdf85343091a9131
Lucca Ianez <111288789+LuckInzz@users.noreply.github.com>
2025-03-12 11:57:30 -0300

    Att

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,62 +1,13 @@
 // Função para exibir o formulário de login
-function showLogin() {
-    document.getElementById('loginForm').style.display = 'block';
-    document.getElementById('registerForm').style.display = 'none';
-}
+var btnSignIn = document.querySelector("#signin");
+var btnSignUp = document.querySelector("#signup");
 
-// Função para exibir o formulário de cadastro
-function showRegister() {
-    document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('registerForm').style.display = 'block';
-}
+var body = document.querySelector("body");
 
-// Função para fazer login
-function login() {
-    const email = document.getElementById('loginEmail').value;
-    const password = document.getElementById('loginPassword').value;
+btnSignIn.addEventListener("click", function() {
+    body.className = "sign-in-js";
+});
 
-    fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            window.location.href = '/main/index.html';  // Redireciona para a página principal
-        } else {
-            alert('Login falhou!');
-        }
-    });
-}
-
-// Função para fazer cadastro
-function register() {
-    const email = document.getElementById('registerEmail').value;
-    const password = document.getElementById('registerPassword').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-
-    if (password !== confirmPassword) {
-        alert('As senhas não coincidem!');
-        return;
-    }
-
-    fetch('http://localhost:8080/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Cadastro bem-sucedido!');
-            showLogin();  // Exibe o formulário de login após cadastro
-        } else {
-            alert('Cadastro falhou!');
-        }
-    });
-}
+btnSignUp.addEventListener("click", function() {
+    body.className = "sign-up-js";
+});
